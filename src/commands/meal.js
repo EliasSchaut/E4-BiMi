ff8d07b9c12428dce3a69c09e88a8e6ce86753cd
EliasSchaut <eschaut@outlook.de>
2022-06-13 17:00:53 +0200

    add boilerplate for meal command

---
+++ src/commands/meal.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    name: 'meal',
+    description: 'Create a floor meal to automatically calculate the depts of the floor',
+    disabled: false,
+    admin_only: true,
+    e4_group_only: true,
+    execute(ctx) {
+        ctx.reply('This command is not implemented yet!')
+
+    }
+}
